eba9256e93538a0892f2003c8d6e09354da031a5
andresrdt <andres.rodriguez-de@mail.escuelaing.edu.co>
2019-01-28 20:08:43 -0500

    commit readme incompleto

--- src/main/java/com/mycompany/Punto45/server.java
+++ src/main/java/com/mycompany/Punto45/server.java
@@ -7,19 +7,35 @@ package com.mycompany.Punto45;
 
 import java.net.*;
 import java.io.*;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author 2112076
  */
 public class server {
-     public static void main(String[] args) throws IOException {
+    private static Socket clientSocket;
+    public static void main(String[] args) throws IOException {
+        
         ServerSocket serverSocket = null;
+        Integer port;
         try {
-            serverSocket = new ServerSocket(35000);
+            port = new Integer(System.getenv("PORT"));
+        } catch (Exception e) {
+            port = 35000;
+        }
+
+        try {
+            serverSocket = new ServerSocket(port);
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
         }
+
         Socket clientSocket = null;
         while (true) {
             try {
@@ -29,45 +45,84 @@ public class server {
                 System.err.println("Accept failed.");
                 System.exit(1);
             }
+            server.clientSocket = clientSocket;
+            cargador();
+        }
+
+    }
+
+    public static void cargador() {
+        try {
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader( clientSocket.getInputStream()));
             String inputLine, outputLine;
             String inputu = null;
+            String format = null;
+            String data = null;
+            byte[] bytes = null;
             while ((inputLine = in.readLine()) != null) {
                 try {
-                    if (inputLine.endsWith(".html")) {
-                        if (!inputLine.startsWith("GET / ")) {
-                            inputu = inputLine;
-                        }
+                    if (inputLine.startsWith("GET")) {
+                        inputu = inputLine;
                     }
                 } catch (java.lang.StringIndexOutOfBoundsException e) {
                 }
+                //System.out.println(inputLine);
                 if (!in.ready()) {
                     break;
                 }
             }
             if (inputu != null) {
-                inputu = inputu.split(" ")[1];
-                File f = new File(inputu.substring(1));
-                BufferedReader entrada;
+                String[] temp;
+                temp = inputu.split(" ");
                 String flag = "";
                 try {
-                    entrada = new BufferedReader(new FileReader(f));
-                    while (entrada.ready()) {
-                        flag += entrada.readLine();
-                    }
+                    if (temp[1].endsWith(".html")) {
+                        bytes = Files.readAllBytes(new File("./" + temp[1].substring(1)).toPath());
+                        data = "" + bytes.length;
+                        format = "text/html";
+                    } else /*if (temp[1].endsWith("png"))*/ {
+                        bytes = Files.readAllBytes(new File("./" + temp[1].substring(1)).toPath());
+                        data = "" + bytes.length;
+                        format = "image/png";
+                    }/** else {
+                        bytes = Files.readAllBytes(new File("./error.html").toPath());
+                        data = "" + bytes.length;
+                        format = "text/html";
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                    }**/
+                    } catch (NoSuchFileException e) {
+                    bytes = Files.readAllBytes(new File("./error.html").toPath());
+                    data = "" + bytes.length;
+                    format = "text/html";
+                    }
                 inputu = flag;
             }
             outputLine = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type: text/html\r\n"
+                    + "Content-Type: "
+                    + format
                     + "\r\n"
-                    + inputu
-                    + inputLine;
-            out.println(outputLine);
+                    + "Content-Length: "
+                    + data
+                    + "\r\n\r\n";
+            byte[] bite = outputLine.getBytes();
+            try {
+                byte[] salida = new byte[bytes.length + bite.length];
+                for (int i = 0; i < bite.length; i++) {
+                    salida[i] = bite[i];
+                }
+                for (int i = bite.length; i < bite.length + bytes.length; i++) {
+                    salida[i] = bytes[i - bite.length];
+                }
+                clientSocket.getOutputStream().write(salida);
+            } catch (java.lang.NullPointerException e) {
+
+            }
+            clientSocket.close();
+
+        } catch (IOException e) {
+            Logger.getLogger(server.class.getName()).log(Level.SEVERE, null, e);
         }
+
     }
 }
